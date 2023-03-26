4abeac064f526027a89f0559763f471432ee723e
NikitaKrasnov12 <116708902+NikitaKrasnov12@users.noreply.github.com>
2023-03-26 16:31:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,8 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i; j < len; j++) {
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 count++;
             }
@@ -74,7 +74,7 @@ int cbinsearch(int* arr, int size, int value) {
 
 int countPairs3(int* arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len - 1; i++) {
+    for (int i = 0; i < len - 1 && arr[i] <= value; i++) {
         count += cbinsearch(arr + i, len - i, value - arr[i]);
     }
     if (count > 0) {
